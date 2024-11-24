1fe092fc2851a7010e30bbcb1cbbefb490d184dc
XxJipow <jiprcroyale@gmail.com>
2024-11-24 20:40:00 +0000

    add actor

---
+++ actor/bridge/src/ext/ops.d.ts
@@ -0,0 +1,27 @@
+/**
+ * This file contains types for Tivet operations.
+ */
+
+declare module "ext:core/ops" {
+	import type { InKey, OutKey, OutEntry, ListQuery } from "internal_types";
+
+	export function op_tivet_kv_get(key: InKey): Promise<OutEntry | null>;
+	export function op_tivet_kv_get_batch(
+		keys: InKey[],
+	): Promise<Array<OutKey, OutEntry>>;
+	export function op_tivet_kv_list(
+		query: ListQuery,
+		reverse: boolean,
+		limit?: number,
+	): Promise<Array<OutKey, OutEntry>>;
+	export function op_tivet_kv_put(
+		key: InKey,
+		value: Uint8Array,
+	): Promise<void>;
+	export function op_tivet_kv_put_batch(
+		entries: Map<InKey, Uint8Array>,
+	): Promise<void>;
+	export function op_tivet_kv_delete(key: InKey): Promise<void>;
+	export function op_tivet_kv_delete_batch(keys: InKey[]): Promise<void>;
+	export function op_tivet_kv_delete_all(): Promise<void>;
+}
